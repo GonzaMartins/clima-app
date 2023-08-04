8e08d856d4a0bd391d05a0d4ecced5ed3b6eef1a
gonzaMartins <gonzalom02@hotmail.com>
2023-08-04 18:19:54 -0300

    console.log({weather}) commit clima-app

--- src/components/weatherApp.js
+++ src/components/weatherApp.js
@@ -1,54 +1,61 @@
 // import process from 'process'
-import {useState, useEffect} from 'react'
-import WeatherForm from './weatherForm'
-import WeatherMainInfo from './weatherMainInfo'
-import Error from './error'
-import '../styles/weatherApp.css'
-import Loading from './loading'
-import nubes from '../img/nubes.png'
+import { useState, useEffect } from "react";
+import WeatherForm from "./weatherForm";
+import WeatherMainInfo from "./weatherMainInfo";
+import Error from "./error";
+import "../styles/weatherApp.css";
+import Loading from "./loading";
+import nubes from "../img/nubes.png";
 
+export default function WeatherApp() {
+  const [weather, setWeather] = useState(null);
+  const [weatherIcon, setWeatherIcon] = useState(null);
 
-export default function WeatherApp (){
+  useEffect(() => {
+    document.title = `Clima | ${weather?.name ?? ""} `;
+  }, [weather]);
 
-    const [weather, setWeather] = useState(null)
-    const [weatherIcon, setWeatherIcon] = useState(null)
-
-
-    useEffect(() => {
-        loadInfo()
-    }, [loadInfo])
-
-    useEffect(() => {
-        document.title = `Clima | ${weather?.name ?? ''} `
-    }, [weather])
-
-    console.log({weather})
-    const loadInfo = async (city = 'Buenos Aires') =>{
-        try {
-            const request = await fetch(`${process.env?.REACT_APP_URL}&q=${city}`)
-            const json = await request.json()
-            setWeather(json)
-            const icon = await fetch(`${process.env?.REACT_APP_ICON}${json.weather[0].icon}.png`)
-            setWeatherIcon(icon)
-            console.log(weatherIcon)
-        } catch (error) {
-            console.log(error)
-        }
+  const loadInfo = async (city = "Buenos Aires") => {
+    try {
+      const request = await fetch(`${process.env?.REACT_APP_URL}&q=${city}`);
+      const json = await request.json();
+      setWeather(json);
+      const icon = await fetch(
+        `${process.env?.REACT_APP_ICON}${json.weather[0].icon}.png`
+      );
+      setWeatherIcon(icon);
+      console.log(weatherIcon);
+    } catch (error) {
+      console.log(error);
     }
-    console.log({weather})
-        const handleChangeCity = (city) => {
-        setWeather(null)
-        loadInfo(city)
-    }
-    return (
-    <div className='divContainer'>
-    <div className='weatherContainer'>
-        <div className='divHeader'>
-            <div className='titulo'>Climapp</div>
-            <img className='numeImg' alt='nubes' src={nubes} />
+  };
+  const handleChangeCity = (city) => {
+    setWeather(null);
+    loadInfo(city);
+  };
+
+  useEffect(() => {
+    loadInfo();
+  }, [loadInfo]);
+  return (
+    <div className="divContainer">
+      <div className="weatherContainer">
+        <div className="divHeader">
+          <div className="titulo">Climapp</div>
+          <img className="numeImg" alt="nubes" src={nubes} />
         </div>
         <WeatherForm onChangeCity={handleChangeCity} />
-        { weather && weather.cod === '404' ? <Error /> : weather ? <WeatherMainInfo weather={weather} icon={weatherIcon}/> : <div className='spinnerContainer'> <Loading /> </div> }
+        {weather && weather.cod === "404" ? (
+          <Error />
+        ) : weather ? (
+          <WeatherMainInfo weather={weather} icon={weatherIcon} />
+        ) : (
+          <div className="spinnerContainer">
+            {" "}
+            <Loading />{" "}
+          </div>
+        )}
+      </div>
     </div>
-    </div>)
+  );
 }
